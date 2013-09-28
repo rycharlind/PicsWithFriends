88cf6ebdd937704b3c5cb1d34287e6a4646ec95d
Ryan Lindbeck <rycharlind@Ryans-iMac.local>
2013-09-27 22:58:27 -0500

    Initial Commit

---
+++ PicsWithFriends/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PicsWithFriends
+//
+//  Created by Ryan Lindbeck on 9/27/13.
+//  Copyright (c) 2013 Inndevers LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PicsWithFriends/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PicsWithFriends
+//
+//  Created by Ryan Lindbeck on 9/27/13.
+//  Copyright (c) 2013 Inndevers LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
